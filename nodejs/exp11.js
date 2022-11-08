628c698a74f5276d6276ae5100b61bd35e23cfd7
aniket162002 <aniketsshinde@acpce.ac.in>
2022-11-08 15:47:54 +0530

    second

---
+++ nodejs/exp11.js
@@ -0,0 +1,9 @@
+const calculate = require("basic-calculator-js")
+ 
+var x = 50
+var y = 50
+ 
+console.log(calculate.multiply(x, y));
+console.log(calculate.subtract(x, y));
+console.log(calculate.add(x,y));
+console.log(calculate.divide(x, y));
